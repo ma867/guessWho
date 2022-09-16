7d72fd65b32c6f43e4a66e5c89f50413e155d830
ma867 <ma867@njit.edu>
2022-09-16 00:57:25 -0400

    part two of board logic

--- js/app.js
+++ js/app.js
@@ -8,15 +8,19 @@ const getStartedBtn = document.querySelector("#get-started")
 const startGameBtn = document.querySelector("#start-game")
 const board = document.querySelector("#chris-board")
 const gameConsole = document.querySelector("#game-controls")
+const guessCounter = document.querySelector("#guess-count > span")
+const questionCounter = document.querySelector("#question-count > span")
 const menu = document.querySelector(".menu")
 const tutorial = document.querySelector("#modal-textbox")
 const chrises = document.querySelectorAll(".square")
+const askButtons = document.querySelectorAll(".ask button")
+const answers = document.querySelector("#answers > span")
 
-
+console.log(guessCounter,questionCounter)
 
 
 /* ======================
-CREATE Tamagotchi
+CLASSES
 =========================*/
 class Chris{
     constructor(lastName, sex, alive = true, profession, ethnicity, hairColor){
@@ -29,37 +33,61 @@ class Chris{
     }
     
 }
+
+class Player{
+    constructor(guessCount = 3, questionCount = 0){
+        this.guessCount = guessCount
+        this.questionCount = questionCount
+        this.currentChrises = [
+            "hemsworth", "columbus", "rock", "pang", "jenner", "ronaldo",
+            "aguilera", "cavallari", "brown", "christ", "milian", "pratt",
+            "castro", "ludacris", "teigen", "yang", "stewart", "wallace",
+            "evans", "hendricks", "tucker", "roldan", "wiig", "paul",
+        ]
+
+    }
+    updateGuessCount(){
+        this.guessCount--
+        guessCounter.textContent = this.guessCount
+    }
+    updateQuestionCount(){
+        this.questionCount++
+        questionCounter.textContent = this.questionCount
+    }
+}
 /* ======================
 GLOBAL VARS
 =========================*/
+let computedChris = ""
+let player = new Player()
 const chrisList = [
     new Chris("hemsworth", "male", true, ["actor"], "caucasian", "blond"),
-    new Chris("columbus", "male", false, ["historic figure"], "caucasian", "brown"),
-    new Chris("rock", "male", true, ["actor", "comedian"], "african-american", "brown"),
-    new Chris("pang", "male", true, ["actor"], "asian", "brown"),
-    new Chris("jenner", "female", true, ["tv personality"], "caucasian", "brown"),
+    new Chris("columbus", "male", false, ["historic-figure"], "caucasian", "brunette"),
+    new Chris("rock", "male", true, ["actor", "comedian"], "african-american", "brunette"),
+    new Chris("pang", "male", true, ["actor"], "asian", "brunette"),
+    new Chris("jenner", "female", true, ["tv-personality"], "caucasian", "brunette"),
     new Chris("aguilera", "female", true, ["actor", "singer"], "latino", "blond"),
-    new Chris("cavallari", "female", true, ["tv personality"], "caucasian", "blond"),
+    new Chris("cavallari", "female", true, ["tv-personality"], "caucasian", "blond"),
     new Chris("brown", "male", true, ["singer"], "african-american", "blond"),
-    new Chris("christ", "male", false, ["historic figure"], "asian", "brown"),
-    new Chris("milian", "female", true, ["actor"], "latino", "brown"),
-    new Chris("pratt", "male", true, ["actor"], "caucasian", "brown"),
-    new Chris("castro", "male", true, ["singer"], "latino", "brown"),
-    new Chris("ludacris", "male", true,["actor", "singer"], "african-american", "brown"),
-    new Chris("teigen", "female", true, ["tv personality"], "asian", "brown"),
-    new Chris("yang", "female", true, ["doctor","tv personality"], "asian", "brown"),
-    new Chris("stewart", "female", true, ["actor"], "caucasian", "brown"),
-    new Chris("wallace", "male", false, ["singer"], "african-american", "brown"),
-    new Chris("evans", "male", true, ["actor"], "caucasian", "brown"),
+    new Chris("christ", "male", false, ["historic-figure"], "asian", "brunette"),
+    new Chris("milian", "female", true, ["actor"], "latino", "brunette"),
+    new Chris("pratt", "male", true, ["actor"], "caucasian", "brunette"),
+    new Chris("castro", "male", true, ["singer"], "latino", "brunette"),
+    new Chris("ludacris", "male", true,["actor", "singer"], "african-american", "brunette"),
+    new Chris("teigen", "female", true, ["tv-personality"], "asian", "brunette"),
+    new Chris("yang", "female", true, ["doctor","tv-personality"], "asian", "brunette"),
+    new Chris("stewart", "female", true, ["actor"], "caucasian", "brunette"),
+    new Chris("wallace", "male", false, ["singer"], "african-american", "brunette"),
+    new Chris("evans", "male", true, ["actor"], "caucasian", "brunette"),
     new Chris("hendricks", "female", true, ["actor"], "caucasian", "red"),
-    new Chris("tucker", "male", true, ["actor", "comedian"], "african-american", "brown"),
-    new Chris("roldan", "male", true, ["athlete"], "latino", "brown"),
+    new Chris("tucker", "male", true, ["actor", "comedian"], "african-american", "brunette"),
+    new Chris("roldan", "male", true, ["athlete"], "latino", "brunette"),
     new Chris("wiig", "female", true, ["actor", "comedian"], "caucasian", "blond"),
-    new Chris("paul", "male", true, ["athlete"], "african-american", "brown"),
+    new Chris("paul", "male", true, ["athlete"], "african-american", "brunette"),
 ]
 
 console.log(chrisList)
-
+console.log(player)
 /* =============================
 FUNCTIONS
 ============================= */
@@ -67,6 +95,9 @@ const startTutorial = () => {
     console.log("yerr")
     menu.style.display = "none"
     tutorial.style.display = "block"
+    computedChris = randomizeChris()
+    console.log(computedChris)
+    // console.log(askButtons)
 
 }
 
@@ -85,10 +116,11 @@ const setupGame = () =>{
 
 }
 
-// const whichChris = (chris) => {
-//     console.log("clicked on chris")
-//     console.log(evt.getAttribute("id"))
-// }
+const randomizeChris = () =>{
+    return  chrisList[Math.floor(Math.random() * 24)];
+}
+
+
 
 /* =============================
 EVENT LISTENERS
@@ -99,12 +131,15 @@ getStartedBtn.addEventListener("click", startTutorial)
 startGameBtn.addEventListener("click", startGame)
 
 
+
 chrises.forEach((chris) =>{
     chris.addEventListener('dblclick', function(){
-        console.log("clicked on chris")
+        // console.log("clicked on chris")
 
       
         let selectedChris = document.getElementById(chris.id)
+        let index = player.currentChrises.indexOf(chris.id)
+        console.log(index)
         let chrisLastName = selectedChris.querySelector(".name")
         let chrisImage = selectedChris.querySelector(".image")
 
@@ -113,12 +148,184 @@ chrises.forEach((chris) =>{
             chrisImage.style.display = "initial"
             chrisLastName.style.backgroundColor = "aliceblue"
             chrisLastName.style.color = "#094f8f"
+            player.currentChrises.push(chris.id)
+            console.log(player.currentChrises)
+            
 
         } 
         else{
             chrisImage.style.display = "none"
             chrisLastName.style.backgroundColor = "#d3232c"
             chrisLastName.style.color = "aliceblue"
+            player.currentChrises.splice(index,1)
+            console.log(player.currentChrises)
+            
+        }
+
+    })
+})
+
+askButtons.forEach((question) =>{
+    question.addEventListener('click', function(){
+  
+        let selectedQuestion = question.id
+        let professions = computedChris.profession
+        let selectedButton = document.getElementById(selectedQuestion)
+        selectedButton.disabled = true
+        selectedButton.style.backgroundColor = "#c7c7c7"
+        selectedButton.style.color = "#c7c7c7"
+        player.updateQuestionCount()
+        console.log(selectedButton)
+        console.log(professions)
+        console.log(selectedQuestion)
+        
+        switch(selectedQuestion){
+            case "female":
+                if(computedChris.sex === "female"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                
+                break
+            case "male":
+                if(computedChris.sex === "male"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "alive":
+                if(computedChris.alive){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "singer":
+                if(professions.includes("singer")){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "actor":
+                console.log("actor")
+                if(professions.includes("actor")){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "tv-personality":
+                console.log("tv personality")
+                if(professions.includes("tv-personality")){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "doctor":
+                console.log("doctor")
+                if(professions.includes("doctor")){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "comedian":
+                console.log("comedian")
+                if(professions.includes("comedian")){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "historic-figure":
+                console.log("historic-figure")
+                if(professions.includes("historic-figure")){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "african-american":
+                console.log("african-american")
+                if(computedChris.ethnicity === "african-american"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                
+                break
+            case "latino":
+                console.log("latino")
+                console.log("latino")
+                if(computedChris.ethnicity === "latino"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "asian":
+                console.log("asian")
+               
+                if(computedChris.ethnicity === "asian"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "caucasian":
+                console.log("caucasian")
+               
+                if(computedChris.ethnicity === "caucasian"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "blond":
+                console.log("blond")
+                
+                if(computedChris.hairColor === "blond"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "brunette":
+                console.log("brunette")
+                if(computedChris.hairColor === "brunette"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
+            case "red":
+                console.log("red")
+                if(computedChris.hairColor === "red"){
+                    answers.textContent = "Yes"
+                }
+                else{
+                    answers.textContent = "No!"
+                }
+                break
         }
 
     })
